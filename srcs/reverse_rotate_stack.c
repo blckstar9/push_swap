c747050c4ff23734dfa861a4e5cffe043cededc4
blckstar9 <belaouchiayoub@gmail.com>
2025-03-16 17:41:07 +0100

    added all the operations needed for the project, working now on the sorting algorithm

---
+++ srcs/reverse_rotate_stack.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate_stack.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aybelaou <aybelaou@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 17:24:58 by aybelaou          #+#    #+#             */
+/*   Updated: 2025/03/16 17:31:44 by aybelaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+static void	reverse_rotate_stack(t_stack **stack)
+{
+	if (!*stack || (*stack)->prev == *stack)
+		return ;
+	*stack = (*stack)->prev;
+}
+
+void	reverse_rotate_a(t_stack **stack_a)
+{
+	reverse_rotate_stack(stack_a);
+	ft_putstr_fd("rra\n", 1);
+}
+
+void	reverse_rotate_b(t_stack **stack_b)
+{
+	reverse_rotate_stack(stack_b);
+	ft_putstr_fd("rrb\n", 1);
+}
+
+void	reverse_rotate_both(t_stack **stack_a, t_stack **stack_b)
+{
+	reverse_rotate_stack(stack_a);
+	reverse_rotate_stack(stack_b);
+	ft_putstr_fd("rrr\n", 1);
+}
---
+++ srcs/rotate_stack.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_stack.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aybelaou <aybelaou@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 17:14:06 by aybelaou          #+#    #+#             */
+/*   Updated: 2025/03/16 17:17:42 by aybelaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+static void	rotate_stack(t_stack **stack)
+{
+    if (!*stack || (*stack)->next == *stack)
+        return ;
+    *stack = (*stack)->next;
+}
+
+void	rotate_a(t_stack **stack_a)
+{
+    rotate_stack(stack_a);
+    ft_putstr_fd("ra\n", 1);
+}
+
+void	rotate_b(t_stack **stack_b)
+{
+    rotate_stack(stack_b);
+    ft_putstr_fd("rb\n", 1);
+}
+
+void	rotate_both(t_stack **stack_a, t_stack **stack_b)
+{
+    rotate_stack(stack_a);
+    rotate_stack(stack_b);
+    ft_putstr_fd("rr\n", 1);
+}
